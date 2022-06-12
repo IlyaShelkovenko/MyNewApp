bb6b94089bb7d12fa545beea3a16d839e21c760b
IlyaShelkovenko <hostov47@gmail.com>
2022-06-12 11:06:39 +0300

    Init commit

---
+++ app/src/main/java/com/gmail/hostov47/mynewapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.gmail.hostov47.mynewapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.gmail.hostov47.mynewapp.ui.theme.MyNewAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyNewAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyNewAppTheme {
+        Greeting("Android")
+    }
+}
